c2d709ee2bd9e9607fff71613f01cdd27a263810
IvySandi <61860575+IvySandi@users.noreply.github.com>
2020-11-26 21:40:26 +0630

    Add files via upload

---
+++ getwh.js
@@ -0,0 +1,10 @@
+function getWidthandHeight(){
+var w_width = $(window).width();
+var w_height = $(window).height();
+var wh = {
+    width:  w_width,
+    height: w_height
+    
+  };
+  return wh;
+}
